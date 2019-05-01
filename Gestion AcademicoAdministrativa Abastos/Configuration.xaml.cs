83f2223e435a592f539f70e434502fb416066c1c
Pepe Fabra <jofaval@outlook.com>
2019-05-01 22:51:48 +0200

    Add Configuration Panel

---
+++ Gestion AcademicoAdministrativa Abastos/Configuration.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Posibles_Interfaces_de_Usuario
+{
+    /// <summary>
+    /// Lógica de interacción para Configuration.xaml
+    /// </summary>
+    public partial class Configuration : Window
+    {
+        public Configuration()
+        {
+            InitializeComponent();
+        }
+    }
+}
