5e2d92b98074c4e3b733259a0fb73855d7dfb45f
Pepe Fabra <jofaval@outlook.com>
2019-05-03 16:45:41 +0200

    Add ViewEnum

---
+++ EntityFrameworkModel/DataStructure/ViewsEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum ViewsEnum
+    {
+        ALUMNO = 0,
+        PROFESOR = 1,
+        ADMINISTRATIVO = 2,
+        ADMINISTRADOR = 3,
+    }
+}
