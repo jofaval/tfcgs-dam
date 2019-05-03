deefedac1e5ed3e70eca86470e7ba00f11381d84
Pepe Fabra <jofaval@outlook.com>
2019-05-03 22:24:55 +0200

    Add WindowSizeEnum

---
+++ EntityFrameworkModel/DataStructure/WindowSizeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum WindowSizeEnum
+    {
+        WIDTH_1920_X_HEIGHT_1080 = 0,
+        WIDTH_1600_X_HEIGHT_900 = 1,
+        WIDTH_1440_X_HEIGHT_900 = 2,
+        WIDTH_1280_X_HEIGHT_720 = 3,
+        WIDTH_1152_X_HEIGHT_864 = 4,
+        WIDTH_1024_X_HEIGHT_768 = 5,
+    }
+}
