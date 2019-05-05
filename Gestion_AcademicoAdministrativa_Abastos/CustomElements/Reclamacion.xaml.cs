3c924f58b73d104e152a94ac6b88b65391fd765f
Pepe Fabra <jofaval@outlook.com>
2019-05-05 18:49:33 +0200

    Modify Reclamacion Style

--- Gestion_AcademicoAdministrativa_Abastos/CustomElements/Reclamacion.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/CustomElements/Reclamacion.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,11 @@ namespace Gestion_AcademicoAdministrativa_Abastos.CustomElements
         public Reclamacion()
         {
             InitializeComponent();
+        }
+
+        public static Grid CreateReclamacion(Reclamacion reclamacion)
+        {
+            return null;
         }
     }
 }
