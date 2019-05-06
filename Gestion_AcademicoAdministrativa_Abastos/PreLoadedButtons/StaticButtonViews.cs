f64a99b09039489ce8db2fe98262da65c215c6f0
Pepe Fabra <jofaval@outlook.com>
2019-05-06 08:42:07 +0200

    Add StaticButtonViews

---
+++ Gestion_AcademicoAdministrativa_Abastos/PreLoadedButtons/StaticButtonViews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Gestion_AcademicoAdministrativa_Abastos.Buttons
+{
+    static public class StaticButtonViews
+    {
+        static public List<Button> AlumnoView = new List<Button>
+        {
+            new Button()
+            {
+
+            },
+        };
+    }
+}
