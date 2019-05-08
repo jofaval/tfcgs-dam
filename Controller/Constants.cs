07e55ffa82818aaefcd0eed739eb9e5e357327b7
Pepe Fabra <jofaval@outlook.com>
2019-05-09 00:54:34 +0200

    Add String Joiner Constant

--- Controller/Constants.cs
+++ Controller/Constants.cs
@@ -14,6 +14,7 @@ namespace Controller
 
         public static readonly string UnsuccesfulLogIn = "No se ha podido acceder. ¡Lo sentimos!" +
                     "\nLa combinación usuario/contraseña no era la correcta.";
+        public static readonly string StringJoiner = ", ";
 
         public static readonly string UsernameFile = "user.sav";
         public static readonly string SettingsFile = "user.stgs";
