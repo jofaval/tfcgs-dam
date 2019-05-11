a8a45ce279e6c2ddc31ede23b70d747f411907ac
Pepe Fabra <jofaval@outlook.com>
2019-05-11 18:14:57 +0200

    Add DNILeterEnum To Model

---
+++ EntityFrameworkModel/DataStructure/DNILeterEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum DNILeterEnum
+    {
+        T = 0,
+        R = 1,
+        W = 2,
+        A = 3,
+        G = 4,
+        M = 5,
+        Y = 6,
+        F = 7,
+        P = 8,
+        D = 9,
+        X = 10,
+        B = 11,
+        N = 12,
+        J = 13,
+        Z = 14,
+        S = 15,
+        Q = 16,
+        V = 17,
+        H = 18,
+        L = 19,
+        C = 20,
+        K = 21,
+        W = 22,
+    }
+}
