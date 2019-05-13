d975bdba7fbdd956dad0b928038d1c1f997a1731
Pepe Fabra <jofaval@outlook.com>
2019-05-13 21:03:13 +0200

    Add FormularioAula

---
+++ Gestion_AcademicoAdministrativa_Abastos/FormularioAula.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gestion_AcademicoAdministrativa_Abastos
+{
+    /// <summary>
+    /// Lógica de interacción para FormularioAula.xaml
+    /// </summary>
+    public partial class FormularioAula : Window
+    {
+        public FormularioAula()
+        {
+            InitializeComponent();
+        }
+    }
+}
