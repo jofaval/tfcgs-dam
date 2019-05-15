2fa0d333f69f97154cde998f10255a24eb7b2e65
Pepe Fabra <jofaval@outlook.com>
2019-05-15 08:40:57 +0200

    Add Month Enum

---
+++ EntityFrameworkModel/DataStructure/MonthEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum MonthEnum
+    {
+        UNSET = 0,
+        JANUARY = 1,
+        FEBRUARY = 2,
+        MARCH = 3,
+        APRIL = 4,
+        MAY = 5,
+        JUNE = 6,
+        JULE = 7,
+        AUGUST = 8,
+        SEPTEMBER = 9,
+        OCTOBER = 10,
+        NEVEMBER = 11,
+        DECEMBER = 12,
+    }
+}
