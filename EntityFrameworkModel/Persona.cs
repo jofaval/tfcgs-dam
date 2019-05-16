adf5acd7d96970dc9a9fe98e719375e709f5f8a0
Pepe Fabra <jofaval@outlook.com>
2019-05-16 09:18:04 +0200

    Add Partial Classes

---
+++ EntityFrameworkModel/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Persona : Persona
+    {
+        public override string ToString() => $"DNI: {Dni}, Nombre: {Nombre}, Apellidos: {Apellidos}";
+    }
+}
