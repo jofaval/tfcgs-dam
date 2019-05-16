a4b42328a399b031e0a8d2f7e5526153ebd766ec
Pepe Fabra <jofaval@outlook.com>
2019-05-16 09:01:18 +0200

    Add Some FormularioHorario Functionality

--- Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioHorario.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioHorario.xaml.cs
@@ -23,6 +23,10 @@ namespace Gestion_AcademicoAdministrativa_Abastos
         public FormularioHorario()
         {
             InitializeComponent();
+            var cursos = StaticReferences.Context.CursoDbSet.ToList();
+            ComboBoxCurso.ItemsSource = cursos;
+            //var asignaturas = StaticReferences.Context.AsignaturaDbSet.ToList();
+            //ComboBoxAsignatura.ItemsSource = asignaturas;
         }
 
         private void Create_Click(object sender, RoutedEventArgs e)
@@ -31,22 +35,31 @@ namespace Gestion_AcademicoAdministrativa_Abastos
             var context = StaticReferences.Context;
             var cursos = context.CursoDbSet;
 
-            var cursoCodText = TxtCursoCod.Text;
-            var cursoNombreText = TxtCursoNombre.Text;
-            var curso = cursos.SingleOrDefault(c => c.Cod.Equals(cursoCodText) && c.Nombre.Equals(cursoNombreText));
+            //var cursoCodText = TxtCursoCod.Text;
+            //var cursoNombreText = TxtCursoNombre.Text;
+            //var curso = cursos.SingleOrDefault(c => c.Cod.Equals(cursoCodText) && c.Nombre.Equals(cursoNombreText));
 
             var asignaturas = context.AsignaturaDbSet;
-            var asignaturaCodText = TxtCodAsignatura.Text;
+            var selectedAsignatura = (dynamic)((ComboBoxItem)ComboBoxAsignatura.SelectedItem).Content;
+            var asignaturaCodText = ((string)selectedAsignatura.Cod);
             var asignatura = asignaturas.SingleOrDefault(a => a.Cod.Equals(asignaturaCodText));
+
+            var horaInicio = TxtHoraInicio.Value;
 
             var horario = new Model.Horario()
             {
                 Anyo = AdministrativoFunctionality.GetAcademicYear(StaticReferences.CurrentDateTime),
-                CursoCod = cursoCodText,
-                CursoNombre = cursoNombreText,
-                Curso = curso,
-                Asignatura
+                //CursoCod = cursoCodText,
+                //CursoNombre = cursoNombreText,
+                //Curso = curso,
+                CodAsignatura = asignaturaCodText,
+                Asignatura = asignatura,
             };
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
