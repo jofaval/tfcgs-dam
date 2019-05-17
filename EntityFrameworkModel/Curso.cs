6d04340f4f2d94a91db7965255f4ce0870e900c5
Pepe Fabra <jofaval@outlook.com>
2019-05-17 08:32:19 +0200

    Add Equals to Partial Class Curso

--- EntityFrameworkModel/Curso.cs
+++ EntityFrameworkModel/Curso.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public partial class Curso
+    public partial class Curso : IEquatable<Curso>
     {
         public override string ToString() => $"Codigo: {Cod}, Nombre: {Nombre}";
+
+        public bool Equals(Curso other) => Cod.Equals(other.Cod);
     }
 }
