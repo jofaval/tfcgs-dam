c62417ffcd25ba75c822a8c850df38012664c885
Pepe Fabra <jofaval@outlook.com>
2019-05-18 17:33:18 +0200

    Add ComponentRemover

---
+++ Controller/ComponentRemover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class ComponentRemover
+    {
+        private static ComponentGenerator Instance { get; set; }
+
+        public ComponentGenerator GetInstance()
+        {
+            if (Instance is null)
+            {
+                Instance = new ComponentGenerator();
+            }
+
+            return Instance;
+        }
+
+
+    }
+}
