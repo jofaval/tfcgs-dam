515fd04442b154be03772168f3de4f6838294c83
Pepe Fabra <jofaval@outlook.com>
2019-05-18 15:02:01 +0200

    Add Equals To Horario Partial Class

---
+++ EntityFrameworkModel/Horario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Horario
+    {
+        public bool Equals(Horario other) => Curso.Equals(other.Curso)
+            && Asignatura.Equals(other.Asignatura)
+            && HoraFinal.Equals(other.HoraFinal)
+            && HoraInicio.Equals(other.HoraInicio)
+            && Dia.Equals(other.Dia);
+    }
+}
