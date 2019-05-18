cb452b95f08e15d05668f6c7565cdfdc9592426e
Pepe Fabra <jofaval@outlook.com>
2019-05-18 17:31:12 +0200

    Add Create Functionality

--- Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioPersona.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioPersona.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Controller;
 
 namespace Gestion_AcademicoAdministrativa_Abastos
 {
@@ -25,6 +26,18 @@ namespace Gestion_AcademicoAdministrativa_Abastos
             var buscadorPersona = new BuscadorV2();
             XamlFunctionality.ReplaceGrids(ModifyTab, buscadorPersona.ContentPanel);
             buscadorPersona.Close();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            var dni = TxtDNI.Text;
+            var nif = TxtNIF.Text;
+            var nombre = TxtNombre.Text;
+            var apellidos = TxtApellidos.Text;
+            var email = TxtEmail.Text;
+            var telefono = TxtTelefono.Text;
+
+            Notification.CreateNotificaion(ComponentGenerator.GetInstance().CreatePersona(dni, nif, nombre, apellidos, email, telefono));
         }
     }
 }
