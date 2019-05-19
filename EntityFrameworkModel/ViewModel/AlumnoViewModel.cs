c0f67f2cd7a8d461ed48aadc68d754777d9b9218
Pepe Fabra <jofaval@outlook.com>
2019-05-19 20:06:45 +0200

    Add AlumnoViewModel

---
+++ EntityFrameworkModel/ViewModel/AlumnoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class AlumnoViewModel : IHasName
+    {
+        public string NumExpediente { get; set; }
+        public string CursoNombre { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+    }
+}
