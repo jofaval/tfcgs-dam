0e553b1d356ccaa7b865a7cb47c9f284609d15bd
Pepe Fabra <jofaval@outlook.com>
2019-05-19 20:02:57 +0200

    Add ProfesorViewModel

---
+++ EntityFrameworkModel/ViewModel/ProfesorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class ProfesorViewModel : IHasName
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public Departamento Departamento { get; set; }
+        public string Funcion { get; set; }
+        public string Email { get; set; }
+    }
+}
