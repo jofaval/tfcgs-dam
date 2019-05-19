35a201bfc4468cfc64395e8315eb64e9cfbe3c67
Pepe Fabra <jofaval@outlook.com>
2019-05-19 20:04:25 +0200

    Add PersonaViewModel

---
+++ EntityFrameworkModel/ViewModel/PersonaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class PersonaViewModel : IHasName
+    {
+        public string Dni { get; set; }
+        public string Nif { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+    }
+}
