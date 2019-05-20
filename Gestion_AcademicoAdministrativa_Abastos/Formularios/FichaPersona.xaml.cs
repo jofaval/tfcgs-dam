7a7b92ef7f1109cb8bb47590278162b9c814b76f
Pepe Fabra <jofaval@outlook.com>
2019-05-20 08:40:33 +0200

    Add Persona Creation In FichaPersona and Direccion Fields

--- Gestion_AcademicoAdministrativa_Abastos/Formularios/FichaPersona.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Formularios/FichaPersona.xaml.cs
@@ -58,7 +58,39 @@ namespace Gestion_AcademicoAdministrativa_Abastos.Formularios
 
         private void Create_Click(object sender, RoutedEventArgs e)
         {
+            var dni = TxtDNI.Text;
+            var nif = TxtNIF.Text;
+            var nombre = TxtNombre.Text;
+            var apellidos = TxtApellidos.Text;
+            var email = TxtEmail.Text;
 
+            if (string.IsNullOrWhiteSpace(dni))
+            {
+                Notification.CreateNotificaion("El campo DNI es obligatorio");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(nif))
+            {
+                Notification.CreateNotificaion("El campo NIF es obligatorio");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(nombre))
+            {
+                Notification.CreateNotificaion("El campo Nombre es obligatorio");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(apellidos))
+            {
+                Notification.CreateNotificaion("El campo Apellidos es obligatorio");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(email))
+            {
+                Notification.CreateNotificaion("El campo Email es obligatorio");
+                return;
+            }
+
+            Notification.CreateNotificaion(ComponentGenerator.GetInstance().CreatePersona(dni, nif, nombre, apellidos, email));
         }
 
         private void Modify_Click(object sender, RoutedEventArgs e)
