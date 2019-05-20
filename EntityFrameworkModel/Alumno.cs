9d0204ca3b4db3d6aae0bcde0496cc76bb2865d4
Pepe Fabra <jofaval@outlook.com>
2019-05-20 21:00:35 +0200

    Add Alumno Partial Class

---
+++ EntityFrameworkModel/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Alumno : IEquatable<Alumno>
+    {
+        public bool Equals(Alumno other) => Persona1.Equals(other.Persona1)
+            && NumExpediente.Equals(other.NumExpediente);
+    }
+}
