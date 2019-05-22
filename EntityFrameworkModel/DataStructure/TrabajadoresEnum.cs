0cb14e9f67943425a92e92b72840505c3279735a
Pepe Fabra <jofaval@outlook.com>
2019-05-22 09:33:54 +0200

    Add TrabajadoresEnum

---
+++ EntityFrameworkModel/DataStructure/TrabajadoresEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum TrabajadoresEnum
+    {
+        Profesor = 0,
+        Administrativo = 1,
+        Especial = 2,
+        Mantenimiento = 3,
+    }
+}
