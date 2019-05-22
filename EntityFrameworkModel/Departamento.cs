5046ded351bf3f33bfb8c312e903441f03ed093c
Pepe Fabra <jofaval@outlook.com>
2019-05-22 14:41:23 +0200

    Add Departamento Partial Class

---
+++ EntityFrameworkModel/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Departamento : IEquatable<Departamento>
+    {
+        public bool Equals(Departamento other) => Cod.Equals(other.Cod);
+
+        public override string ToString() => $"Codigo: {Cod}, Nombre: {Nombre}";
+    }
+}
