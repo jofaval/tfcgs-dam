74b60aa8b557f4f518634629df0722a7f9455ce8
Pepe Fabra <jofaval@outlook.com>
2019-05-24 20:37:43 +0200

    Add WeekEnum

---
+++ EntityFrameworkModel/DataStructure/WeekEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum WeekEnum
+    {
+        Lunes = 0,
+        Martes = 1,
+        Miercoles = 2,
+        Jueves = 3,
+        Viernes = 4,
+    }
+}
