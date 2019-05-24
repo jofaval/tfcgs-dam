828a60681834ea7bcbae171e7448e7dccd97df4f
Pepe Fabra <jofaval@outlook.com>
2019-05-24 17:29:55 +0200

    Fix Name

---
+++ EntityFrameworkModel/ViewModel/OrdenadorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class OrdenadorViewModel
+    {
+        public string Num { get; set; }
+        public string Piso { get; set; }
+        public string Estado { get; set; }
+        public string Ip { get; set; }
+        public string CodOrdenadorAula { get; set; }
+        public string SistemaOperativo { get; set; }
+    }
+}
