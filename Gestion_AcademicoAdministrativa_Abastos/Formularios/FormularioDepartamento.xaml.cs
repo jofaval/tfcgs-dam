66d7a4df8395d6d909aa1c6084f86a6ca44914d5
Pepe Fabra <jofaval@outlook.com>
2019-05-24 19:16:49 +0200

    Add Formulario Departamento

--- Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioDepartamento.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioDepartamento.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Controller;
 using Model;
 
 namespace Gestion_AcademicoAdministrativa_Abastos
@@ -23,6 +24,69 @@ namespace Gestion_AcademicoAdministrativa_Abastos
         public FormularioDepartamento()
         {
             InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            var cod = TxtCod.Text;
+            var context = StaticReferences.Context;
+            if (context.DepartamentoDbSet
+                .Any(d => d.Cod.Equals(cod)))
+            {
+                Notification.CreateNotificaion("Ya existe");
+                return;
+            }
+            var nombre = TxtNombre.Text;
+
+            var departamento = new Departamento()
+            {
+                Cod = cod,
+                Nombre = nombre,
+            };
+
+            context.DepartamentoDbSet.Add(departamento);
+            context.SaveChanges();
+
+            Notification.CreateNotificaion("Se ha creado con exito");
+        }
+
+        private void Modify_Click(object sender, RoutedEventArgs e)
+        {
+            var cod = TxtCod.Text;
+            var context = StaticReferences.Context;
+            var departamento = context.DepartamentoDbSet
+                .SingleOrDefault(d => d.Cod.Equals(cod));
+            if (departamento is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado el registro");
+                return;
+            }
+            var nombre = TxtNombre.Text;
+
+            departamento.Nombre = nombre;
+
+            context.Entry(departamento).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+
+            Notification.CreateNotificaion("Se ha modificado con exito");
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            var cod = TxtCod.Text;
+            var context = StaticReferences.Context;
+            var departamento = context.DepartamentoDbSet
+                .SingleOrDefault(d => d.Cod.Equals(cod));
+            if (departamento is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado el registro");
+                return;
+            }
+
+            context.DepartamentoDbSet.Remove(departamento);
+            context.SaveChanges();
+
+            Notification.CreateNotificaion("Se ha modificado con exito");
         }
     }
 }
