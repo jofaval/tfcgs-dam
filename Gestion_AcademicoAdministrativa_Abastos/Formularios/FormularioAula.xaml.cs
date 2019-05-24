83585c7d2f5ef8564ac747a81603fe5f967998c6
Pepe Fabra <jofaval@outlook.com>
2019-05-24 09:12:46 +0200

    Add FormAula Ordenador Functionality

--- Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioAula.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioAula.xaml.cs
@@ -58,9 +58,120 @@ namespace Gestion_AcademicoAdministrativa_Abastos
                 .SingleOrDefault(a => a.Num.Equals(num)
                 && a.Piso.Equals(piso));
 
-            if (context.AulaDbSet.Contains(aula))
+            if (aula is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado");
+            }
+            else
             {
                 context.AulaDbSet.Remove(aula);
+                context.SaveChanges();
+                Notification.CreateNotificaion("Borrado con exito");
+            }
+        }
+
+        private void CreateOrdenador_Click(object sender, RoutedEventArgs e)
+        {
+            var context = StaticReferences.Context;
+
+            var piso = TxtPiso.Text;
+            var num = TxtNum.Text;
+
+            var aula = context.AulaDbSet
+                .SingleOrDefault(a => a.Num.Equals(num)
+                && a.Piso.Equals(piso));
+
+            if (aula is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado el aula");
+                return;
+            }
+
+            var cod = TxtCod.Text;
+            var estado = TxtEstado.Text;
+            var ip = TxtIP.Text;
+            var sistemaOperativo = TxtSO.Text;
+
+            if (context.OrdenadorDbSet.Any(o => o.Num.Equals(num)
+                && o.Piso.Equals(piso)
+                && o.CodOrdenadorAula.Equals(cod)))
+            {
+                Notification.CreateNotificaion("Ya existe");
+                return;
+            }
+            var ordenador = new Ordenador()
+            {
+                Piso = piso,
+                Num = num,
+                CodOrdenadorAula = cod,
+                Estado = estado,
+                Ip = ip,
+                SistemaOperativo = sistemaOperativo,
+                Aula = aula,
+            };
+            context.OrdenadorDbSet.Add(ordenador);
+            context.SaveChanges();
+            Notification.CreateNotificaion("Se ha creado con exito");
+        }
+
+        private void ModifyOrdenador_Click(object sender, RoutedEventArgs e)
+        {
+            var context = StaticReferences.Context;
+
+            var piso = TxtPiso.Text;
+            var num = TxtNum.Text;
+
+            var aula = context.AulaDbSet
+                .SingleOrDefault(a => a.Num.Equals(num)
+                && a.Piso.Equals(piso));
+
+            if (aula is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado el aula");
+                return;
+            }
+
+            var cod = TxtCod.Text;
+            var estado = TxtEstado.Text;
+            var ip = TxtIP.Text;
+            var sistemaOperativo = TxtSO.Text;
+            var ordenador = context.OrdenadorDbSet.SingleOrDefault(o => o.Num.Equals(num)
+                && o.Piso.Equals(piso)
+                && o.CodOrdenadorAula.Equals(cod));
+
+            if (ordenador is null)
+            {
+                Notification.CreateNotificaion("No es ha encontrado el ordenador");
+                return;
+            }
+
+            ordenador.Estado = estado;
+            ordenador.Ip = ip;
+            ordenador.SistemaOperativo = sistemaOperativo;
+
+            context.Entry(ordenador).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+
+            Notification.CreateNotificaion("Se ha modificado con exito");
+        }
+
+        private void DeleteOrdenador_Click(object sender, RoutedEventArgs e)
+        {
+            var piso = TxtPiso.Text;
+            var num = TxtNum.Text;
+            var cod = TxtCod.Text;
+
+            var context = StaticReferences.Context;
+            var ordenador = context.OrdenadorDbSet
+                .SingleOrDefault(o => o.Num.Equals(num)
+                && o.Piso.Equals(piso)
+                && o.CodOrdenadorAula.Equals(cod));
+
+            if (context.OrdenadorDbSet.Any(o => o.Num.Equals(num)
+                && o.Piso.Equals(piso)
+                && o.CodOrdenadorAula.Equals(cod)))
+            {
+                context.OrdenadorDbSet.Remove(ordenador);
                 context.SaveChanges();
                 Notification.CreateNotificaion("Borrado con exito");
             }
