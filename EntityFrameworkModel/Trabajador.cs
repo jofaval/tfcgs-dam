c9f06fa6bf2fd8f7edb3a521bd2ca1cb7781c79d
Pepe Fabra <jofaval@outlook.com>
2019-05-25 21:55:16 +0200

    Add Trabajador Partial Class And Append NombreCompleto Method

---
+++ EntityFrameworkModel/Trabajador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Trabajador
+    {
+        public string NombreCompleto()
+        {
+            return $"{Persona1.Nombre}, {Persona1.Apellidos}";
+        }
+    }
+}
