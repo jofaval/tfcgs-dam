1f12fa8498f59045e09965ce969d88f6a01684ce
Pepe Fabra <jofaval@outlook.com>
2019-05-26 00:24:39 +0200

    Add ChangePassword Functionality And View

--- Gestion_AcademicoAdministrativa_Abastos/ChangePassword.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/ChangePassword.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,55 @@ namespace Gestion_AcademicoAdministrativa_Abastos
     /// </summary>
     public partial class ChangePassword : Window
     {
+        public const int DiviedBy = 25;
+
         public ChangePassword()
         {
             InitializeComponent();
+        }
+
+        private void TxtNewPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox senderAsTextBox)
+            {
+                StrengthLevel.Value = DataIntegrityChecker.CheckPasswordStrengthLevel(senderAsTextBox.Text) * DiviedBy;
+            }
+        }
+
+        private void BtnChange_Click(object sender, RoutedEventArgs e)
+        {
+            var user = XamlBridge.CurrentUser;
+            var oldPassword = Cryptography.Decrypt(user.Contrasenya, Constants.EncryptationKey);
+
+            if (!oldPassword.Equals(TxtOldPassword.Password))
+            {
+                Notification.CreateNotificaion("La contraseña antigua no es correcta");
+                return;
+            }
+            else
+            {
+                var newPassword = TxtNewPassword.Password;
+
+                if (!newPassword.Equals(TxtNewPasswordRepeated.Password))
+                {
+                    Notification.CreateNotificaion("La contraseñas no coinciden");
+                    return;
+                }
+                else
+                {
+                    if (StrengthLevel.Value/ DiviedBy > 2)
+                    {
+                        Notification.CreateNotificaion("La contraseña es débil");
+                        return;
+                    }
+                    else
+                    {
+                        user.Contrasenya = Cryptography.Decrypt(newPassword, user.Username);
+                        Notification.CreateNotificaion("La contraseña se ha cambiado correctamente");
+                    }
+                }
+            }
+
         }
     }
 }
