6f1785ab55ecd09ee34acb822a8c957562b1a588
Pepe Fabra <jofaval@outlook.com>
2019-05-25 19:35:06 +0200

    Add Profesor Partial Class

---
+++ EntityFrameworkModel/Profesor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Profesor
+    {
+        public override string ToString()
+        {
+            var persona = Trabajador1.Persona1;
+            return $"Nombre: {persona.Nombre}, Apellidos: {persona.Apellidos}, eMail: {persona.Email}";
+        }
+    }
+}
