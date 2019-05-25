7e60c5b910492f32a7f98c5109c0a4ef0eac1aaf
Pepe Fabra <jofaval@outlook.com>
2019-05-25 23:27:28 +0200

    Add Public Partial Class Telefono

---
+++ EntityFrameworkModel/Telefono.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Telefono : IEquatable<Telefono>
+    {
+        public bool Equals(Telefono other) => Persona1.Equals(other.Persona1)
+            && Telefono1.Equals(other.Telefono1);
+    }
+}
