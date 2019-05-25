a63bf9a72bcc1bd6f464cc131cf13f3dd1fe3451
Pepe Fabra <jofaval@outlook.com>
2019-05-25 18:54:49 +0200

    Add Estudio Partial Class

---
+++ EntityFrameworkModel/Estudio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Estudio : IEquatable<Estudio>
+    {
+        public bool Equals(Estudio other) => Curso.Equals(other.Curso)
+                && Alumno1.Equals(other.Alumno1)
+                && Anyo.Equals(other.Anyo);
+    }
+}
