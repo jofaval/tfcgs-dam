0bc173927b91bd9f377e66160d1d4f244a83c5e5
Pepe Fabra <jofaval@outlook.com>
2019-05-25 21:49:31 +0200

    Add FormularioImpartimiento Implementation In FormularioHorario

--- Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioHorario.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Formularios/FormularioHorario.xaml.cs
@@ -52,26 +52,7 @@ namespace Gestion_AcademicoAdministrativa_Abastos
 
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            var selectedCurso = (Curso)ComboBoxCurso.SelectedValue;
-
-            var selectedAsignatura = (Asignatura)ComboBoxAsignatura.SelectedValue;
-            var asignaturaCodText = selectedAsignatura.Cod;
-
-            var horaInicio = TxtHoraInicio.Value.Value;
-            var horaFinal = TxtHoraFinal.Value.Value;
-
-            var day = (byte)(WeekEnum)ComboBoxDia.SelectedValue;
-
-            var horario = new Model.Horario()
-            {
-                Anyo = AdministrativoFunctionality.GetAcademicYear(StaticReferences.CurrentDateTime),
-                CursoCod = selectedCurso.Cod,
-                CursoNombre = selectedCurso.Nombre,
-                CodAsignatura = asignaturaCodText,
-                HoraInicio = horaInicio,
-                HoraFinal = horaFinal,
-                Dia = day,
-            };
+            var horario = GetHorario();
 
             var context = StaticReferences.Context;
             if (context.HorarioDbSet.AsEnumerable().Contains(horario))
@@ -88,17 +69,101 @@ namespace Gestion_AcademicoAdministrativa_Abastos
 
         private void CreateImpartimiento_Click(object sender, RoutedEventArgs e)
         {
+            var horario = GetHorario();
+            if (horario is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado el horario");
+                return;
+            }
+
+            var selectedProfesor = (Profesor)ComboBoxProfesores.SelectedValue;
+            var selectedAula = (Aula)ComboBoxAulas.SelectedValue;
+
             var impartimiento = new Impartimiento()
             {
-
+                Profesor1 = selectedProfesor,
+                Profesor = selectedProfesor.Trabajador,
+                Aula = selectedAula,
+                AulaNum = selectedAula.Num,
+                AulaPiso = selectedAula.Piso,
+                Anyo = horario.Anyo,
+                CursoCod = horario.CursoCod,
+                CursoNombre = horario.CursoNombre,
+                CodAsignatura = horario.CodAsignatura,
+                HoraInicio = horario.HoraInicio,
+                HoraFinal = horario.HoraFinal,
+                Dia = horario.Dia,
+                Horario = horario,
             };
 
-            StaticReferences.Context.ImpartimientoDbSet.AsEnumerable().Contains(impartimiento);
+            if (StaticReferences.Context.ImpartimientoDbSet.AsEnumerable().Contains(impartimiento))
+            {
+                Notification.CreateNotificaion("Ya existe");
+            }
+            else
+            {
+                StaticReferences.Context.ImpartimientoDbSet.Add(impartimiento);
+                StaticReferences.Context.SaveChanges();
+                Notification.CreateNotificaion("Se ha creado con exito");
+            }
+
         }
 
         private void DeleteImpartimiento_Click(object sender, RoutedEventArgs e)
         {
+            var horario = GetHorario();
+            if (horario is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado el horario");
+                return;
+            }
 
+            var selectedProfesor = (Profesor)ComboBoxProfesores.SelectedValue;
+            var selectedAula = (Aula)ComboBoxAulas.SelectedValue;
+
+            var impartimiento = StaticReferences.Context.ImpartimientoDbSet
+                .AsEnumerable()
+                .SingleOrDefault(i =>
+                i.Profesor1.Equals(selectedProfesor)
+                && i.Aula.Equals(selectedAula)
+                && i.Horario.Equals(horario)
+            );
+
+            if (impartimiento is null)
+            {
+                Notification.CreateNotificaion("No se ha encontrado registro");
+            }
+            else
+            {
+                StaticReferences.Context.ImpartimientoDbSet.Remove(impartimiento);
+                StaticReferences.Context.SaveChanges();
+                Notification.CreateNotificaion("Se ha borrado con exito");
+            }
+        }
+
+        public Model.Horario GetHorario()
+        {
+            var selectedCurso = (Curso)ComboBoxCurso.SelectedValue;
+
+            var selectedAsignatura = (Asignatura)ComboBoxAsignatura.SelectedValue;
+            var asignaturaCodText = selectedAsignatura.Cod;
+
+            var horaInicio = TxtHoraInicio.Value.Value;
+            var horaFinal = TxtHoraFinal.Value.Value;
+
+            var day = (byte)(WeekEnum)ComboBoxDia.SelectedValue;
+
+            return StaticReferences.Context.HorarioDbSet
+                .AsEnumerable()
+            .SingleOrDefault(
+                h => h.Anyo.Equals(AdministrativoFunctionality.GetAcademicYear(StaticReferences.CurrentDateTime))
+               && h.CursoCod.Equals(selectedCurso.Cod)
+               && h.CursoNombre.Equals(selectedCurso.Nombre)
+               && h.CodAsignatura.Equals(asignaturaCodText)
+               && h.HoraInicio.Equals(horaInicio)
+               && h.HoraFinal.Equals(horaFinal)
+               && h.Dia.Equals(day)
+            );
         }
     }
 }
