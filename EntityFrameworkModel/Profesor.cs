1f362d3a07244c42bd66726ee318b4b42de707b6
Pepe Fabra <jofaval@outlook.com>
2019-05-25 20:07:54 +0200

    Implement IEquatable In Profesor Partial Class

--- EntityFrameworkModel/Profesor.cs
+++ EntityFrameworkModel/Profesor.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public partial class Profesor
+    public partial class Profesor : IEquatable<Profesor>
     {
+        public bool Equals(Profesor other) => Trabajador1.Persona1.Equals(other.Trabajador1.Persona1);
+
         public override string ToString()
         {
             var persona = Trabajador1.Persona1;
