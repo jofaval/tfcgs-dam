77d9fa6cd5c00e9965716dea02fb84a1653c23db
Pepe Fabra <jofaval@outlook.com>
2019-05-26 01:26:07 +0200

    Add PermisosUsuarioViewModel Class

---
+++ EntityFrameworkModel/ViewModel/PermisosUsuarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class PermisosUsuarioViewModel
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool PermisoAdmin { get; set; }
+        public bool PermisoAdministrativo { get; set; }
+        public bool PermisProfesor { get; set; }
+        public bool PermisoAlumno { get; set; }
+    }
+}
