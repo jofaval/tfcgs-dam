14b705a1879f239c25cc2c96873f4f34b43db9ab
Pepe Fabra <jofaval@outlook.com>
2019-05-26 02:27:36 +0200

    Add Reclamacion Functionality

--- Gestion_AcademicoAdministrativa_Abastos/Reclamaciones.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/Reclamaciones.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Controller;
 using Model;
 
 namespace Gestion_AcademicoAdministrativa_Abastos
@@ -20,15 +21,50 @@ namespace Gestion_AcademicoAdministrativa_Abastos
     /// </summary>
     public partial class Reclamaciones : Window
     {
+        public List<Reclamacion> Todas { get; set; }
+        public List<Reclamacion> EnTramite { get; set; }
+        public List<Reclamacion> Tramitadas { get; set; }
+
         public Reclamaciones()
         {
             InitializeComponent();
-
+            ComboBoxProfesor.ItemsSource = StaticReferences.Context.ProfesorDbSet.ToList();
+            var reclamaciones = StaticReferences.Context.ReclamacionDbSet.ToList();
+            Todas = reclamaciones;
+            DataGridTodas.ItemsSource = Todas;
+            EnTramite = reclamaciones
+                .Where(r => r.EnTramite.HasValue)
+                .ToList();
+            DataGridEnTramite.ItemsSource = EnTramite;
+            Tramitadas = reclamaciones
+                .Where(r => r.FechaRevision.HasValue)
+                .ToList();
+            DataGridTodas.ItemsSource = Tramitadas;
         }
 
         private void CreateReclamacion_Click(object sender, RoutedEventArgs e)
         {
+            var asunto = TxtAsunto.Text;
+            var selectedProfesor = (Profesor)ComboBoxProfesor.SelectedValue;
+            var contenido = TxtAsunto.Text;
+            var alumno = XamlBridge.CurrentUser.Persona1.Alumno;
+            var numParte = alumno.Reclamacion.Count;
 
+            var reclamacion = new Reclamacion()
+            {
+                Asunto = asunto,
+                Alumno = alumno.Persona,
+                Alumno1 = alumno,
+                Contenido = contenido,
+                NumParte = numParte,
+                DirigidoA = selectedProfesor.Trabajador,
+                FechaEnvio = DateTime.Now,
+            };
+
+            StaticReferences.Context.ReclamacionDbSet.Add(reclamacion);
+            //Todas.Add(reclamacion);
+            StaticReferences.Context.SaveChanges();
+            Notification.CreateNotificaion("Se ha creado con exito");
         }
     }
 }
