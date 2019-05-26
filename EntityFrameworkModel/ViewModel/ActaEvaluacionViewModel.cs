2e6fff494c7eb841a8c3fa01d9436453c3edea01
Pepe Fabra <jofaval@outlook.com>
2019-05-26 16:21:47 +0200

    Add ActasEvaluacionViewModel To ViewModel

---
+++ EntityFrameworkModel/ViewModel/ActaEvaluacionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class ActaEvaluacionViewModel
+    {
+        public DateTime Fecha { get; set; }
+        public Profesor Profesor { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
