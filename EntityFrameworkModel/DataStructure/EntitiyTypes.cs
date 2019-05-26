10399ca7ee3aafbf04718692390b38cefc8bd21c
Pepe Fabra <jofaval@outlook.com>
2019-05-26 18:43:47 +0200

    Add EntityTypes

---
+++ EntityFrameworkModel/DataStructure/EntitiyTypes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DataStructure
+{
+    public enum EntitiyTypes
+    {
+        Administrativo = 0,
+        Alumno = 1,
+        Asignatura = 2,
+        AsistenciaDiaAsignatura = 3,
+        AsistenciaSemanaAsignatura = 4,
+        Aula = 5,
+        Bachiller = 6,
+        Baja = 7,
+        Certificado = 8,
+        CertificadoMatricula = 9,
+        CertificadoNotas = 10,
+        CertificadoTitulo = 11,
+        Ciclo = 12,
+        Convocatoria = 13,
+        Curso = 14,
+        Departamento = 15,
+        Eso = 16,
+        Especial = 17,
+        Evaluacion = 18,
+        Horario = 19,
+        Impartimiento = 20,
+        Mantenimiento = 21,
+        Nota = 22,
+        Ordenador = 23,
+        PermisosUsuario = 24,
+        Persona = 25,
+        Profesor = 26,
+        ProfesorSustituto = 27,
+        Proyecto = 28,
+        Reclamacion = 29,
+        Superior = 30,
+        Telefono = 31,
+        Titulo = 32,
+        Trabajador = 33,
+        Usuario = 34,
+        Tutores = 35,
+        Estudio = 36,
+        ActasEvaluacion = 37,
+    }
+}
