68cc95309a5b8031262db2f7f357d77eeb906d67
Pepe Fabra <jofaval@outlook.com>
2019-05-27 08:54:47 +0200

    Add ReclamacionViewModel

---
+++ EntityFrameworkModel/ViewModel/ReclamacionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class ReclamacionViewModel
+    {
+        public string Asunto { get; set; }
+        public string Contenido { get; set; }
+        public int NumParte { get; set; }
+        public string DirigidoA { get; set; }
+    }
+}
