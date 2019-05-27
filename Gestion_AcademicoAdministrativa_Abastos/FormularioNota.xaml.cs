c00d2b0dfd3d529e405452eebe37d26a15b5d810
Pepe Fabra <jofaval@outlook.com>
2019-05-27 23:40:49 +0200

    FormularioNota Now Adds Nota

--- Gestion_AcademicoAdministrativa_Abastos/FormularioNota.xaml.cs
+++ Gestion_AcademicoAdministrativa_Abastos/FormularioNota.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controller;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Model;
+using System.Globalization;
 
 namespace Gestion_AcademicoAdministrativa_Abastos
 {
@@ -19,10 +23,83 @@ namespace Gestion_AcademicoAdministrativa_Abastos
     /// </summary>
     public partial class FormularioNota : Window
     {
+        public Asignatura SelectedAsignatura { get; set; }
+
         public FormularioNota()
         {
             InitializeComponent();
             //new Alumno().Convocatoria.Last().Evaluacion.Last().Nota
+            var profesor = XamlBridge.CurrentUser.Persona1.Trabajador.Profesor;
+            var list = ProfesorFunctionality.GetAsignaturasImpartidas(profesor).ToList();
+
+            ComboBoxAsignatura.ItemsSource = list;
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedAlumno = (Alumno) ComboBoxAlumno.SelectedValue;
+            var selectedCurso = selectedAlumno.Estudio.Last().Curso;
+
+            var convocatoria = new Convocatoria()
+            {
+                Alumno = selectedAlumno.Persona,
+                Alumno1 = selectedAlumno,
+                Curso = selectedCurso,
+                CursoCod = selectedCurso.Cod,
+                CursoNombre = selectedCurso.Nombre,
+                Num = 1,
+                CodAsignatura = SelectedAsignatura.Cod,
+                Asignatura = SelectedAsignatura,
+            };
+            StaticReferences.Context.ConvocatoriaDbSet.Add(convocatoria);
+            StaticReferences.Context.SaveChanges();
+
+            var evaluacion = new Evaluacion()
+            {
+                Alumno = selectedAlumno.Persona,
+                CursoCod = selectedCurso.Cod,
+                CursoNombre = selectedCurso.Nombre,
+                EvaluacionNum = 1,
+                ConvocatoriaNum = 1,
+                Convocatoria = convocatoria,
+                CodAsignatura = SelectedAsignatura.Cod,
+            };
+            StaticReferences.Context.EvaluacionDbSet.Add(evaluacion);
+            StaticReferences.Context.SaveChanges();
+
+            var nota = new Nota()
+            {
+                Evaluacion = null,
+                Alumno = selectedAlumno.Persona,
+                CodAsignatura = SelectedAsignatura.Cod,
+                CursoCod = selectedCurso.Cod,
+                CursoNombre = selectedCurso.Nombre,
+                Observaciones = TxtObservaciones.Text,
+                Valoracion = double.Parse(TxtNota.Text, CultureInfo.InvariantCulture),
+            };
+
+            evaluacion.Nota = nota;
+            StaticReferences.Context.Entry(evaluacion).State = System.Data.Entity.EntityState.Modified;
+            StaticReferences.Context.SaveChanges();
+        }
+
+        private void Modify_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxAsignatura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedAsignatura = (Asignatura)ComboBoxAsignatura.SelectedValue;
+            var list = ProfesorFunctionality.GetAlumnosWhereAsignaturasImpartidas(selectedAsignatura).ToList();
+
+            ComboBoxAlumno.ItemsSource = list;
+            SelectedAsignatura = selectedAsignatura;
         }
     }
 }
