f2822f8a098502a055db1cdd1640bbc3d75f56f7
Pepe Fabra <jofaval@outlook.com>
2019-05-28 01:16:53 +0200

    Add NotasViewModel

---
+++ EntityFrameworkModel/ViewModel/NotasViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class NotasViewModel
+    {
+        public string Curso { get; set; }
+        public string Asignatura { get; set; }
+        public double Valoracion { get; set; }
+        public double Observaciones { get; set; }
+        public int Evaluacion { get; set; }
+    }
+}
