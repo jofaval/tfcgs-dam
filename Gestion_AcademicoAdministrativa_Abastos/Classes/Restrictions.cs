56a96e66cadefb9fc15f38659b5a8ff440f9008e
Pepe Fabra <jofaval@outlook.com>
2019-06-02 00:27:41 +0200

    Add Restriction class

---
+++ Gestion_AcademicoAdministrativa_Abastos/Classes/Restrictions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gestion_AcademicoAdministrativa_Abastos.Classes
+{
+    public static class Restrictions
+    {
+        public static void AlphaNumericText(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.Any(x => char.IsLetterOrDigit(x) || x.Equals(' '));
+        }
+    }
+}
